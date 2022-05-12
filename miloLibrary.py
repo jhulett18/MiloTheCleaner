c6ef1d9ddfdde826c5b97f5f117622c41e1f3836
JT <iitacitus@gmail.com>
2022-05-12 14:43:44 -0400

    Added multi-level folders for AUDIO and BOOKS for Organization

--- miloLibrary.py
+++ miloLibrary.py
@@ -16,6 +16,9 @@ def findBooks():
 
 		# Grabbing User's Download dir
 		dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Books"
+
+		BOOK_dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Books\\Books"
+		DOC_dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Books\\Documents"
 		source = "C:\\Users\\"+ user +"\\Downloads\\" + file  
 		 
 
@@ -23,20 +26,30 @@ def findBooks():
 		if not os.path.exists(dest):
 			os.makedirs(dest)
 
+		if not os.path.exists(BOOK_dest):
+			os.makedirs(BOOK_dest)
+
+		if not os.path.exists(DOC_dest):
+			os.makedirs(DOC_dest)
+
+		
+
+
+
 
 		# Move epub 
 		if file.endswith(".epub"):
 			print("EPUB: " + file + " has been moved successfully!") 
-			shutil.move(source, dest)
+			shutil.move(source, BOOK_dest)
 
 		# Move pdf 
 		if file.endswith(".pdf"):
 			print("PDF: " + file + " has been moved successfully!") 
-			shutil.move(source, dest)
+			shutil.move(source, DOC_dest)
 
 		if file.endswith(".txt"):
-			print("PDF: " + file + " has been moved successfully!") 
-			shutil.move(source, dest)
+			print("TXT: " + file + " has been moved successfully!") 
+			shutil.move(source, DOC_dest)
 
 			
 def findAudio():
@@ -47,25 +60,38 @@ def findAudio():
 	for file in list:
 
 		# Variables
-		
 		dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Audio"
+		MP3_dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Audio\\MP3"
+		MP4_dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Audio\\MP4"
+		WAV_dest = "C:\\Users\\"+ user +"\\Downloads\\Milo_Audio\\WAV"
 		source = "C:\\Users\\"+ user +"\\Downloads\\" + file 
 		 
 
 		# Create New Folder if it does not exist
+		
 		if not os.path.exists(dest):
 			os.makedirs(dest)
+
+		if not os.path.exists(MP3_dest):
+			os.makedirs(MP3_dest)
+
+		if not os.path.exists(MP4_dest):
+			os.makedirs(MP4_dest)
+
+		if not os.path.exists(WAV_dest):
+			os.makedirs(WAV_dest)
+
 
 
 		if file.endswith(".mp3"): 
 			print("MP3: " + file + " has been moved successfully!")
-			shutil.move(source, dest)
+			shutil.move(source, MP3_dest)
 		if file.endswith(".mp4"):
 			print("MP4: " + file + " has been moved successfully!") 
-			shutil.move(source, dest)
+			shutil.move(source, MP4_dest)
 		if file.endswith(".wav"): 
 			print("WAV: " + file + " has been moved successfully!")
-			shutil.move(source, dest)
+			shutil.move(source, WAV_dest)
 
 		
 
